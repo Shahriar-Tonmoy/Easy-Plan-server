9c977fd0132bd81d5b4d2a60a1a7e47af70ea367
Tonmoy <shahariartonmoy17@gmail.com>
2023-12-22 17:24:29 +0600

    update api added

--- index.js
+++ index.js
@@ -62,6 +62,44 @@ async function run() {
         console.log(result);  
     });
 
+    //Update Data
+    app.put('/tasks/:id', async (req, res) => {
+        const id = req.params.id;
+        const toBeUpdatedProduct = req.body;
+        console.log(toBeUpdatedProduct);
+        const query = { _id: new ObjectId(id)};
+        const options = { upsert: true };
+        const updateTask = {
+          $set: {
+            title:toBeUpdatedProduct.title,
+            description:toBeUpdatedProduct.description,
+            deadline:toBeUpdatedProduct.deadline,
+            priority:toBeUpdatedProduct.priority,
+          }
+        }
+        const result = await tasksCollection.updateOne(query, updateTask, options);
+        res.send(result);
+      })
+
+      //getting one data
+      app.get('/tasks/:id', async (req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const product = await tasksCollection.findOne(query);
+        res.send(product);
+      })
+
+    //delete data
+    app.delete('/tasks/:cid', async(req, res) => {
+        const id  = req.params.cid;
+        console.log(`PLEASE DELETE ID FROM DATABASE: ${id}`);
+        const query = { _id: new ObjectId(id)};
+        console.log(query);
+        
+        const result = await tasksCollection.deleteOne(query);
+        res.send(result);
+      })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
